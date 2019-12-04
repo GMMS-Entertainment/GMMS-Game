9808b7e1720cea8ef4d5a4d2170eb8e1c23f65bf
Marcus Klasenius <58472734+marcusklasenius@users.noreply.github.com>
2019-12-04 11:23:12 +0100

    First version

---
+++ Assets/EarthScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthScript : MonoBehaviour
+{
+    Vector3 touchPosWorld;
+
+    bool rotate = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            touchPosWorld = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            Vector2 touchPosWorld2D = new Vector2(touchPosWorld.x, touchPosWorld.y);
+
+            RaycastHit2D hitInformation = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
+
+            if (hitInformation.collider != null)
+            {
+                rotate = false;
+            }
+        }
+        else
+        {
+            rotate = true;
+        }
+
+        if(rotate)
+        {
+            transform.Rotate(0, 0, 2 * Time.deltaTime);
+        }
+    }
+}
