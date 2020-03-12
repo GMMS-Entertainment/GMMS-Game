eab25520038eca4217a6df5f60a7d120c070f75e
legomasten <gustav.edman01@gmail.com>
2020-03-12 11:05:53 +0100

    Gustav-verison POGGERS!!?

---
+++ Assets/Scripts/MoneyScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MoneyScript : MonoBehaviour
+{
+    private TextMeshProUGUI money;
+    private int balance;
+    private string number;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        money = GetComponent<TextMeshProUGUI>();
+        balance = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        number = string.Format("{0:#,##0}", balance);
+        money.text = "$" + number;
+    }
+
+    public void getMoney()
+    {
+        if (balance < 1000000000)
+        {
+            balance++;
+        }
+    }
+}
