cd4d0c437f8424cd6553e9a96ca4db12d87bd472
makkemaskin <makke.anything@gmail.com>
2020-03-17 15:35:54 +0100

    Added Shop

---
+++ Assets/Scripts/ShopScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopScript : MonoBehaviour
+{
+    public GameObject Shop;
+    public Animator Anim;
+
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+    }
+    public void OpenShop() //Öppnar Shoppen
+    {
+        if (Shop != null)
+        {
+            bool isActive = Shop.activeSelf;
+            Shop.SetActive(!isActive);
+        }
+    }
+}
